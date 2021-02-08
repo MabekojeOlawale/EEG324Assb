c2292e9ce7c9a2132ce290d23796b44ddef80711
MabekojeOlawale <78253543+MabekojeOlawale@users.noreply.github.com>
2021-02-08 04:38:37 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using system.Collection.Generic;
+using System.Linq;
+using System.Text;
+;
+using System.Threading.Tasks;
+
+class miles driven;
+{
+    public double miles = "20 miles";
+{
+    Console.WriteLine("20miles");
+}
+}
+class gallon used;
+{
+    public double gallon = "5 gallons";
+{
+    Console.WriteLine("10 gallons");
+}
+}
+{
+public class program
+    static void Main(string[] args)
+{
+    miles per gallon = (miles driven / gallon used);
+Console.WriteLine("20miles / 5 gallons");
+Console.WriteLine("miles per gallon = 4miles");
+}
+
+}
